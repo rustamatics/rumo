c53094c3957a4670eb23af301e920e5843756467
Bailey Cosier <bailey.cosier@gmail.com>
2017-08-03 17:52:49 +0700

    Cleanup examples target debug

--- examples/simple/target/debug/libsimple.d
+++
@@ -1,1 +0,0 @@
-/home/bailey/Developer/work/droid/examples/simple/target/debug/libsimple.rlib: /home/bailey/Developer/work/droid/examples/simple/src/lib.rs
--- examples/simple/target/debug/main.d
+++
@@ -1,1 +0,0 @@
-/home/bailey/Developer/work/droid/examples/simple/target/debug/main: /home/bailey/Developer/work/droid/examples/simple/src/lib.rs /home/bailey/Developer/work/droid/examples/simple/src/bin/main.rs
