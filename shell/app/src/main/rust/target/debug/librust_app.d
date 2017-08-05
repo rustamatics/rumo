07baeb11d170bf0a949ca83268cc77e52dd51ce4
Bailey Cosier <bailey.cosier@gmail.com>
2017-08-06 05:42:59 +0700

    gitignore updates for build target dirs

--- shell/app/src/main/rust/target/debug/librust_app.d
+++
@@ -1,1 +0,0 @@
-/home/bailey/Developer/work/droid/examples/demo/app/src/main/rust/target/debug/librust_app.so: /home/bailey/Developer/work/droid/examples/demo/app/src/main/rust/src/lib.rs
--- shell/app/src/main/rust/target/release/librust_app.d
+++
@@ -1,1 +0,0 @@
-/home/bailey/Developer/work/droid/examples/demo/app/src/main/rust/target/release/librust_app.so: /home/bailey/Developer/work/droid/examples/demo/app/src/main/rust/src/lib.rs
