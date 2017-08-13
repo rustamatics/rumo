a83c8950f0b5ad018e372081db82e17786d195e4
Bailey Cosier <bailey.cosier@gmail.com>
2017-08-13 23:47:37 +0700

    [add] Config flags for ndk architecture

--- src/config.rs
+++ src/config.rs
@@ -35,7 +35,7 @@ struct TomlAndroid {
 
 pub struct Config {
     /// Path to the root of the Android SDK.
-    pub sdk_path: PathBuf,
+    // pub sdk_path: PathBuf,
     /// Path to the root of the Android NDK.
     pub ndk_path: PathBuf,
 
@@ -57,10 +57,23 @@ pub struct Config {
     /// Versions of this icon with different resolutions have to reside in the res folder
     pub package_icon: Option<String>,
 
+    /// Enable arm target
+    pub enable_arm: bool,
+
+    /// Enable armv7 target
+    pub enable_armv7: bool,
+
+    /// Enable armv7 target
+    pub enable_x86: bool,
+
+    /// Enable mips target
+    pub enable_mips: bool,
+
     /// List of targets to build the app for. Eg. `arm-linux-androideabi`.
     pub build_targets: Vec<String>,
 
-    /// Version of android for which to compile. TODO: ensure that >=18 because Rustc only supports 18+
+    /// Version of android for which to compile.
+    /// TODO: ensure that >=18 because Rustc only supports 18+
     pub android_version: u32,
 
     /// If `Some`, a path that contains the list of assets to ship as part of the package.
@@ -71,7 +84,8 @@ pub struct Config {
     /// If `Some`, a path that contains the list of resources to ship as part of the package.
     ///
     /// The resources can later be loaded with the runtime library.
-    /// This folder contains for example the launcher icon, the styles and resolution dependent images.
+    /// This folder contains for example the launcher icon,
+    /// the styles and resolution dependent images.
     pub res_path: Option<PathBuf>,
 
     /// Should we build in release mode?
@@ -112,29 +126,31 @@ pub fn load(manifest_path: &Path) -> Config {
             content
         };
 
-        let toml  = content.parse::<toml::Value>().unwrap();
+        let toml = content.parse::<toml::Value>().unwrap();
         let decoded: TomlPackage = toml["package"].clone().try_into::<TomlPackage>().unwrap();
 
         let package_name = decoded.name.clone();
         (package_name, decoded.metadata.and_then(|m| m.android))
     };
 
-    let ndk_path = env::var("NDK_HOME").expect("Please set the path to the Android NDK with the \
-                                                $NDK_HOME environment variable.");
+    let ndk_path = env::var("NDK_HOME").expect(
+        "Please set the path to the Android NDK with the \
+                                                $NDK_HOME environment variable.",
+    );
 
-    let sdk_path = {
-        let mut try = env::var("ANDROID_SDK_HOME").ok();
+    // let sdk_path = {
+    //     let mut try = env::var("ANDROID_SDK_HOME").ok();
 
-        if try.is_none() {
-            try = env::var("ANDROID_SDK").ok();
-        }
+    //     if try.is_none() {
+    //         try = env::var("ANDROID_SDK").ok();
+    //     }
 
-        try.expect("Please set the path to the Android SDK with either the $ANDROID_SDK_HOME or \
-                    the $ANDROID_HOME environment variable.")
-    };
+    //     try.expect("Please set the path to the Android SDK with either the $ANDROID_SDK_HOME or \
+    //                 the $ANDROID_HOME environment variable.")
+    // };
 
     let manifest_parent = str::replace(&manifest_path.to_str().unwrap()[..], "/Cargo.toml", "");
-    let project_path =  Path::new(&manifest_parent[..]).to_owned();
+    let project_path = Path::new(&manifest_parent[..]).to_owned();
 
 
     let n = &package_name.clone()[..];
@@ -142,31 +158,64 @@ pub fn load(manifest_path: &Path) -> Config {
 
     // For the moment some fields of the config are dummies.
     Config {
-        sdk_path: Path::new(&sdk_path).to_owned(),
+        // sdk_path: Path::new(&sdk_path).to_owned(),
         ndk_path: Path::new(&ndk_path).to_owned(),
         project_path: project_path,
-        package_name: manifest_content.as_ref().and_then(|a| a.package_name.clone())
-                                       .unwrap_or_else(|| format!("rust.{}", package_name)),
+        package_name: manifest_content
+            .as_ref()
+            .and_then(|a| a.package_name.clone())
+            .unwrap_or_else(|| format!("rust.{}", package_name)),
         project_name: package_name.clone(),
         package_name_sanitized: package_name_sanitized.clone(),
-        package_label: manifest_content.as_ref().and_then(|a| a.label.clone())
-                                       .unwrap_or_else(|| package_name.clone()),
+        package_label: manifest_content
+            .as_ref()
+            .and_then(|a| a.label.clone())
+            .unwrap_or_else(|| package_name.clone()),
         package_icon: manifest_content.as_ref().and_then(|a| a.icon.clone()),
-        build_targets: manifest_content.as_ref().and_then(|a| a.build_targets.clone())
-                                       .unwrap_or(vec!["arm-linux-androideabi".to_owned()]),
-        android_version: manifest_content.as_ref().and_then(|a| a.android_version).unwrap_or(18),
-        assets_path: manifest_content.as_ref().and_then(|a| a.assets.as_ref())
-            .map(|p| manifest_path.parent().unwrap().join(p)),
-        res_path: manifest_content.as_ref().and_then(|a| a.res.as_ref())
-            .map(|p| manifest_path.parent().unwrap().join(p)),
-        release: false,
-        fullscreen: manifest_content.as_ref().and_then(|a| a.fullscreen.clone()).unwrap_or(false),
-        application_attributes: manifest_content.as_ref().and_then(|a| map_to_string(a.application_attributes.clone())),
 
-        activity_attributes: manifest_content.as_ref().and_then(|a| map_to_string(a.activity_attributes.clone())),
+        enable_arm: false,
+        enable_armv7: false,
+        enable_x86: false,
+        enable_mips: false,
+
+        build_targets: manifest_content
+            .as_ref()
+            .and_then(|a| a.build_targets.clone())
+            .unwrap_or(vec!["arm-linux-androideabi".to_owned()]),
+        android_version: manifest_content
+            .as_ref()
+            .and_then(|a| a.android_version)
+            .unwrap_or(18),
+        assets_path: manifest_content
+            .as_ref()
+            .and_then(|a| a.assets.as_ref())
+            .map(|p| manifest_path.parent().unwrap().join(p)),
+        res_path: manifest_content.as_ref().and_then(|a| a.res.as_ref()).map(
+            |p| {
+                manifest_path.parent().unwrap().join(p)
+            },
+        ),
+        release: false,
+        fullscreen: manifest_content
+            .as_ref()
+            .and_then(|a| a.fullscreen.clone())
+            .unwrap_or(false),
+        application_attributes: manifest_content.as_ref().and_then(|a| {
+            map_to_string(a.application_attributes.clone())
+        }),
+
+        activity_attributes: manifest_content.as_ref().and_then(|a| {
+            map_to_string(a.activity_attributes.clone())
+        }),
         target: None,
-        opengles_version_major: manifest_content.as_ref().and_then(|a| a.opengles_version_major).unwrap_or(2),
-        opengles_version_minor: manifest_content.as_ref().and_then(|a| a.opengles_version_minor).unwrap_or(0),
+        opengles_version_major: manifest_content
+            .as_ref()
+            .and_then(|a| a.opengles_version_major)
+            .unwrap_or(2),
+        opengles_version_minor: manifest_content
+            .as_ref()
+            .and_then(|a| a.opengles_version_minor)
+            .unwrap_or(0),
     }
 }
 
