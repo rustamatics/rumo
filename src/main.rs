ea1b8d08d5020894371455a423dee5e747ffc393
Bailey Cosier <bailey.cosier@gmail.com>
2017-08-13 23:46:52 +0700

    [add] Architecture switch controls

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![allow(dead_code)]
 
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 extern crate env_logger;
 
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
@@ -14,7 +16,7 @@ extern crate zip;
 
 use std::path::Path;
 use std::path::PathBuf;
-use std::process::{Command};
+use std::process::Command;
 use clap::{Arg, App, SubCommand};
 
 pub mod config;
@@ -22,11 +24,15 @@ pub mod shell;
 pub mod termcmd;
 pub mod utils;
 pub mod commands;
+pub mod cargo;
+pub mod ndk;
+
+// use config::Config;
 
 #[derive(Debug)]
 enum ManifestLoadError {
     CargoLoadFailure,
-    MissingPath
+    MissingPath,
 }
 
 type ManifestResult = Result<PathBuf, ManifestLoadError>;
@@ -44,36 +50,53 @@ fn main() {
     let mut app = App::new("Kinito")
         .version(VERSION.unwrap_or("unknown"))
         .about(DESC.unwrap_or("--"))
-
-        .arg(Arg::with_name("project-dir")
-             .long("project-dir")
-             .short("d")
-             .help("Override the default project directory provided by `cargo locate-project`")
-             .takes_value(true))
-
-        .arg(Arg::with_name("release")
-             .long("release")
-             .help("Build with cargo `release` mode optimizations"))
-
-        .arg(Arg::with_name("clean")
-             .long("clean")
-             .help("Clean build artifacts for a fresh compilation"))
-
-        .subcommand(SubCommand::with_name("build")
-             .help("Compile Project into a APK\n"))
-
-        .subcommand(SubCommand::with_name("device-install")
-             .help("Install Android APK onto Device\n"));
+        .arg(
+            Arg::with_name("project-dir")
+                .long("project-dir")
+                .short("d")
+                .help(
+                    "Override the default project directory provided by `cargo locate-project`",
+                )
+                .takes_value(true),
+        )
+        .arg(Arg::with_name("release").long("release").help(
+            "Build with cargo `release` mode optimizations",
+        ))
+        .arg(Arg::with_name("clean").long("clean").help(
+            "Clean build artifacts for a fresh compilation",
+        ))
+        .arg(Arg::with_name("arch-arm").long("arch-arm").help(
+            "Build for architecture arm",
+        ))
+        .arg(Arg::with_name("arch-arm").long("arch-arm").help(
+            "Build for architecture arm",
+        ))
+        .subcommand(SubCommand::with_name("build").help(
+            "Compile Project into a APK\n",
+        ))
+        .subcommand(SubCommand::with_name("device-install").help(
+            "Install Android APK onto Device\n",
+        ));
 
     let matches = app.clone().get_matches();
 
     // Fetching the configuration for the build.
     let mut config = config::load(&match matches.value_of("project-dir") {
         Some(p) => PathBuf::from(p),
-        None => { current_manifest_path().unwrap() }
+        None => current_manifest_path().unwrap(),
     });
 
     config.release = matches.is_present("release");
+
+    config.enable_arm = matches.is_present("arch-arm");
+    config.enable_armv7 = matches.is_present("arch-armv7");
+    config.enable_x86 = matches.is_present("arch-x86");
+    config.enable_mips = matches.is_present("arch-mips");
+
+    // Build x86 if no archs specified
+    if !config.enable_arm && !config.enable_armv7 && !config.enable_x86 && !config.enable_mips {
+        config.enable_x86 = true;
+    }
 
     // Provide a way to clean the shells
     matches.is_present("clean") && shell::clean(&config);
@@ -82,14 +105,22 @@ fn main() {
     // Noticeably, this is done after clean has a chance to run.
     shell::embed_if_not_present(&config);
 
+    // Ensure we have access to Standalone NDK toolchains
+    // for each ABI we wish to target
+    ndk::install_standalone(&config);
+
+    // Check to see if .cargo/config is properly setup.
+    // Will install or patch existing config if necessary
+    cargo::install_config(&config);
+
+    ///////////////////////////////////////////////////////////////////
+
+    // Every thing looks good, let's execute the users cli switches.
     if let Some(_) = matches.subcommand_matches("build") {
         commands::build(&config);
-    }
-
-    else if let Some(_) = matches.subcommand_matches("device-install") {
+    } else if let Some(_) = matches.subcommand_matches("device-install") {
         commands::install(&config);
     }
-
     // If we have not matched any sub command at this point,
     // fallback to application help screen.
     else {
@@ -116,7 +147,9 @@ fn current_manifest_path() -> ManifestResult {
     }
 
     #[derive(Deserialize)]
-    struct Data { root: String }
+    struct Data {
+        root: String,
+    }
     let stdout = String::from_utf8(output.stdout).unwrap();
     let decoded: Data = serde_json::from_str(&stdout).unwrap();
     let path = Path::new(&decoded.root).to_owned();
