edb2f85bd839a087dfcf0e05319f5edf36d772cd
Bailey Cosier <bailey.cosier@gmail.com>
2017-08-14 05:11:08 +0700

    [up] shell updates

--- src/cargo.rs
+++ src/cargo.rs
@@ -1,11 +1,46 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::Read;
+
+use toml::Value;
+
 use config::Config;
 
-pub fn install_config(config: &Config) {}
+pub fn install_config(config: &Config) {
+    let config_file = &*format!("{}/.cargo/config", config.project_path_str());
 
-fn install_fresh_config() {}
+    if !Path::new(config_file).exists() {
+        install_fresh_config(config, config_file);
+    } else {
+        if !config.ignore_linker_config {
+            update_existing_config(config, config_file);
+        } else {
+            debug!("Ignoring Linker config")
+        }
+    }
+}
 
-fn update_existing_config() {}
+fn install_fresh_config(config: &Config, file: &str) {
+    debug!("Installing fresh config: .cargo/config")
+}
 
-fn validate_cargo_config() -> bool {
+fn update_existing_config(config: &Config, path: &str) {
+    debug!("Updating existing config: .cargo/config");
+
+    let data = config_contents(path).parse::<Value>().unwrap();
+    println!(".cargo/config\n:#{:?}\n", data)
+}
+
+fn validate_linker_config() -> bool {
     false
+}
+
+fn config_contents(path: &str) -> String {
+    let mut file = File::open(path).expect("Unable to open .cargo/config file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect(
+        "unable to read .cargo/config file",
+    );
+
+    contents
 }
